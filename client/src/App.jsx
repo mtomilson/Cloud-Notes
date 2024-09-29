bbd18aa1e42d1d5c19968ceb8a903e08495e7210
Scott <scottdunphy88@gmail.com>
2024-09-29 03:58:25 -0400

    Modal addition

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './components/Home';
 import ProtectedRoute from './components/ProtectedRoute';
+import Modal from './components/OpenModal';
 
 function App() {
   
@@ -32,6 +33,7 @@ function App() {
           element={
             <ProtectedRoute>
               <Home />
+              <Modal />
             </ProtectedRoute>
           } 
         />
